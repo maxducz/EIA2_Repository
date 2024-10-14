6007be69cf28f6cba21685c128074e6dd99d0f3c
Max Duczmal <deine.email@example.com>
2024-10-14 16:27:54 +0200

    Programmstruktur

--- L001_Gedicht/script.js
+++ L001_Gedicht/script.js
@@ -1,3 +1,17 @@
 "use strict";
 console.log("Expecto Patronum");
+var RandomGedicht;
+(function (RandomGedicht) {
+    let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"];
+    console.log({ subjekte, praedikate, objekte });
+    for (let i = subjekte.length; i >= 1; i--) {
+        let Ergebniss = getVerse([], [], []);
+        console.log(Ergebniss, i);
+    }
+    function getVerse(_subjekte, _praedikate, _objekte) {
+        return "alohomora";
+    }
+})(RandomGedicht || (RandomGedicht = {}));
 //# sourceMappingURL=script.js.map
--- L001_Gedicht/script.ts
+++ L001_Gedicht/script.ts
@@ -1,1 +1,14 @@
 console.log("Expecto Patronum");
+namespace RandomGedicht {
+    let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"]
+    console.log({subjekte, praedikate, objekte})
+    for (let i = subjekte.length; i>= 1; i--) {
+        let Ergebniss: string = getVerse([], [], [])
+        console.log(Ergebniss, i)
+    }
+    function getVerse(_subjekte: string[], _praedikate: string[], _objekte: string[]): string {
+        return "alohomora"
+    }
+}
