bcc5a007bb03da326d6438ed450f9717958c7f94
Max Duczmal <deine.email@example.com>
2024-10-17 21:34:51 +0200

    Fertig A1

--- L001_Gedicht/script.js
+++ L001_Gedicht/script.js
@@ -2,16 +2,24 @@
 console.log("Expecto Patronum");
 var RandomGedicht;
 (function (RandomGedicht) {
-    let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"];
-    console.log({ subjekte, praedikate, objekte });
-    for (let i = subjekte.length; i >= 1; i--) {
-        let Ergebniss = getVerse([], [], []);
-        console.log(Ergebniss, i);
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verbs = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"];
+    console.log({ subjekte: subjects, praedikate: verbs, objekte: objects });
+    let poem = "";
+    for (let i = subjects.length; i > 0; i--) {
+        poem = getVers(subjects, verbs, objects);
+        console.log(poem);
     }
-    function getVerse(_subjekte, _praedikate, _objekte) {
-        return "alohomora";
+    function getVers(_subjects, _verbs, _objects) {
+        let vers = "";
+        let randomsubjects = Math.floor(Math.random() * _subjects.length);
+        let randomverb = Math.floor(Math.random() * _verbs.length);
+        let randomobject = Math.floor(Math.random() * _objects.length);
+        vers += subjects.splice(randomsubjects, 1)[0] + " ";
+        vers += verbs.splice(randomverb, 1)[0] + " ";
+        vers += objects.splice(randomobject, 1)[0] + " ";
+        return vers;
     }
 })(RandomGedicht || (RandomGedicht = {}));
 //# sourceMappingURL=script.js.map
--- L001_Gedicht/script.ts
+++ L001_Gedicht/script.ts
@@ -1,14 +1,33 @@
 console.log("Expecto Patronum");
 namespace RandomGedicht {
-    let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"]
-    console.log({subjekte, praedikate, objekte})
-    for (let i = subjekte.length; i>= 1; i--) {
-        let Ergebniss: string = getVerse([], [], [])
-        console.log(Ergebniss, i)
+    let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"]
+    console.log({ subjekte: subjects, praedikate: verbs, objekte: objects })
+
+
+    let poem: string = "";
+    for (let i = subjects.length; i > 0; i--) {
+        poem = getVers(subjects, verbs, objects);
+        console.log(poem)
     }
-    function getVerse(_subjekte: string[], _praedikate: string[], _objekte: string[]): string {
-        return "alohomora"
+
+
+    function getVers(_subjects: string[], _verbs: string[], _objects: string[]): string {
+        let vers: string = ""
+        let randomsubjects: number = Math.floor(Math.random() * _subjects.length);
+        let randomverb: number = Math.floor(Math.random() * _verbs.length)
+        let randomobject: number = Math.floor(Math.random() * _objects.length);
+        vers += subjects.splice(randomsubjects, 1)[0] + " "
+        vers += verbs.splice(randomverb, 1)[0] + " "
+        vers += objects.splice(randomobject, 1)[0] + " "
+
+
+
+
+        return vers
+
+
+
     }
 }
